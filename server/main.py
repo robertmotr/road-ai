7eaa37184624d3c84831daaee329272b769aa40f
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2023-04-29 20:09:30 -0400

    frame number

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from ProcessImage import ProcessImage
-
+import os
 
 if __name__ == "__main__":
-    image = ProcessImage("crash.mp4", "cars_out.mp4")
+    execution_path = os.getcwd()
+    image = ProcessImage(os.path.join(execution_path, "crash.mp4"), "cars_out.mp4")
     image.detect()
