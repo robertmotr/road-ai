f5cc4ac09714c17c7abeaa013263a48be1ce6051
Arian Sadeghi <arian.sadeghi03@outlook.com>
2023-05-03 13:23:54 -0400

    Removed old lines of code with no use

--- road-ai-website/components/Dropzone/Dropzone.js
+++ road-ai-website/components/Dropzone/Dropzone.js
@@ -3,19 +3,10 @@ import styles from "./Dropzone.module.css"
 
 export default function Dropzone() {
     const selectedFile = useRef();
-    const fileContent = useRef();
-
-    const handleFileRead = (event) => {
-        fileContent.current = btoa(event.target.result);
-    };
 
     const handleSubmit = event => {
         event.preventDefault();
         const file = selectedFile.current.files[0];
-        const reader = new FileReader();
-        reader.onloadend = handleFileRead;
-        reader.readAsBinaryString(file);
-        
         const formData = new FormData();
         formData.append('video', file);
 
