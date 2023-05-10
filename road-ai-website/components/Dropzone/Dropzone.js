b885e3a9f585e1fad3bd7799208963247841826b
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2023-05-10 18:54:59 -0400

    fastapi

--- road-ai-website/components/Dropzone/Dropzone.js
+++ road-ai-website/components/Dropzone/Dropzone.js
@@ -2,30 +2,64 @@ import React, {useRef} from "react";
 import styles from "./Dropzone.module.css"
 
 export default function Dropzone() {
+    const videoElementRef = useRef();
+
     const selectedFile = useRef();
+    const fileContent = useRef();
+
+    const handleFileRead = (event) => {
+        fileContent.current = btoa(event.target.result);
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
         const file = selectedFile.current.files[0];
+        const reader = new FileReader();
+        reader.onloadend = handleFileRead;
+        reader.readAsBinaryString(file);
+        
         const formData = new FormData();
         formData.append('video', file);
-
+        //formData.append('name', file.name);
         // send the JSON data in the request body
-        fetch('http://127.0.0.1:5000/upload-video', {
-            mode: 'no-cors',
+        fetch('http://127.0.0.1:8000/upload-video', {
             method: 'POST',
             body: formData
-        }).catch(console.error);
+        })
+        .then(response => response.json())
+        .then(data => {
+            // access the response data and do something with it
+            const crashed = data.crashed;
+            const frame = data.frame;
+            const videoData = atob(data.encoded_video); // decode the base64-encoded video data
+            console.log(crashed)
+            console.log(frame)
+            console.log(videoData)
+
+            videoElementRef.current.src = 'data:video/mp4;base64,' + videoData;
+
+            videoElementRef.current.play();
+        })
+        .catch(error => {
+            console.log(error)
+            // Handle errors
+        });
     }
 
     return (
-        <form enctype="multipart/form-data" onSubmit={handleSubmit} className={styles.form}>
-            <label>
-                Upload your own video!<br/><br/>
-                <input type="file" name="file" ref={selectedFile} accept="video/*" />
-            </label>
-            <br/><br/>
-            <button className={styles.button} type="submit" >Upload</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label>
+                    Upload your own video!<br/><br/>
+                    <input type="file" name="file" ref={selectedFile} accept="video/*" />
+                </label>
+                <br/><br/>
+                <button className={styles.button} type="submit" >Upload</button>
+            </form>
+            <video width={200} height={200}>
+                <source ref={videoElementRef} type="video/*"></source>
+            </video>
+        </>
+        
     )
 }
--- server/server.py
+++ server/server.py
@@ -1,18 +1,22 @@
-from flask import Flask, request, flash, redirect, url_for, jsonify, send_file
-from io import BytesIO
-from typing import *
-import threading
-import time
-import uuid
-import os
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from ProcessImage import *
+import threading
 import base64
 
-app = Flask(__name__)
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ALLOWED_EXTENSIONS = ['mp4', 'mov', 'avi', '.mkv', '.wmv', '.flv', '.webm', '.gif', '.jpg', '.jpeg', '.png', '.bmp', '.svg', '.mp3', '.wav', '.ogg', '.m4a', '.flac', '.aac', '.wma', '.zip', '.rar', '.tar', '.gz', '.7z', '.bz2', '.xz', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp', '.txt', '.rtf', '.tex', '.wks', '.wps', '.wpd', '.key', '.odf', '.psd', '.ai', '.eps', '.ps', '.svg', '.tiff', '.tif', '.jpg', '.jpeg', '.png', '.bmp', '.gif', '.webp', '.svg', '.ico', '.heic', '.mp4', '.mov', '.avi', '.mkv', '.wmv', '.flv', '.webm', '.mp3', '.wav', '.ogg', '.m4a', '.flac', '.aac', '.wma', '.zip', '.rar', '.tar', '.gz', '.7z', '.bz2', '.xz', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp', '.txt', '.rtf', '.tex', '.wks', '.wps', '.wpd', '.key', '.odf', '.psd', '.ai', '.eps', '.ps', '.svg', '.tiff', '.tif', '.jpg', '.jpeg', '.png', '.bmp', '.gif', '.webp', '.svg', '.ico', '.heic']
-OUTPUT_DESTINATION = os.getcwd() + '\server\output'
-INPUT_DESTINATION = os.getcwd() + '\server\input'
+OUTPUT_DESTINATION = os.getcwd() + '/output'
+INPUT_DESTINATION = os.getcwd() + '/input'
 
 def process_video(inpath: str, outpath:str) -> None:
    event = threading.Event()
@@ -34,57 +38,21 @@ def process_video(inpath: str, outpath:str) -> None:
 def is_valid_file(filename: str) -> bool:
    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload-video', methods=['POST'])
-def upload_handler():
-   if request.method == 'POST':
-      file = request.files['video']
-      print(f"Processing {file.filename}")
+@app.post("/upload-video")
+async def upload_video(video: UploadFile = File(...)):
+    # TODO Error handling things
+    if video.filename and is_valid_file(video.filename):
+         data = video.file.read()
+         with open(os.path.join(INPUT_DESTINATION, video.filename), 'wb') as f:
+            f.write(data)
+         
+         output = process_video(os.path.join(INPUT_DESTINATION, video.filename), os.path.join(OUTPUT_DESTINATION, video.filename.split('.')[0]))
 
-      input_data = file.read()
-      with open(os.path.join(INPUT_DESTINATION, file.filename), 'wb') as f:
-         f.write(input_data)
+         with open(os.path.join(OUTPUT_DESTINATION,  video.filename), 'rb') as f:
+            video_data = f.read()
+            encoded_video = base64.b64encode(video_data).decode()
 
-      # running algorithm on file
-      crashed, crash_frame = process_video(os.path.join(INPUT_DESTINATION, file.filename), os.path.join(OUTPUT_DESTINATION, file.filename.split('.')[0]))
-
-      with open(os.path.join(OUTPUT_DESTINATION, file.filename), 'rb') as f:
-         output_data = f.read()
-      
-      return send_file(BytesIO(output_data), download_name=f"processed-{file.filename}", as_attachment=True)
-   return "Cannot handle this request", 400
-
-   # return send_from_directory(directory=OUTPUT_DESTINATION, path=file.filename, as_attachment=True)
-
-
-   # if request.method == 'POST':
-   #    if 'video' not in request.files:
-   #       return 'No file part', 400
-   #    file = request.files['video']
-   #    if file.filename == '':
-   #       return 'No selected file', 400
-   #    if file.filename and is_valid_file(file.filename):
-   #       data = file.stream.read()  # file.read() ??
-
-   #       with open(os.path.join(INPUT_DESTINATION, file.filename), 'wb') as f:
-   #          f.write(data)
-
-   #       # run algorithm on the file path
-   #       output = process_video(os.path.join(INPUT_DESTINATION, file.filename), os.path.join(OUTPUT_DESTINATION, file.filename.split('.')[0]))
-
-   #       with open(os.path.join(OUTPUT_DESTINATION, file.filename), 'rb') as f:
-   #          video_data = f.read()
-   #          encoded_video = base64.urlsafe_b64encode(video_data).decode()
-
-   #       # when process_video returns, we can finally send back the data to the client
-   #       response_data = {'crashed': output[0], 'frame': output[1], 'encoded_video': encoded_video}
-   #       text = "COLLISION OCCURRED" if output[0] else "NO COLLISION OCCURRED"
-   #       print(f"{text}")
-   #       return jsonify(response_data), 200
-
-   #    else:
-   #       return 'Invalid file type', 400
-   # else:
-   #    return "you shouldn't be sending a get request to this link :)", 400
-
-if __name__ == '__main__':
-   app.run(port=5000, debug=True)
+         response_data = {'crashed': output[0], 'frame': output[1], 'encoded_video': encoded_video}
+         text = "COLLISION OCCURRED" if output[0] else "NO COLLISION OCCURRED"
+         print(f"{text}")
+         return response_data
